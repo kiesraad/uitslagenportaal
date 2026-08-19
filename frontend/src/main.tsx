d1b02e9c43f01be2693cf7daf660feb20fac4bcd
Corniël Joosse <corniel@pixento.nl>
2026-08-19 09:07:31 +0200

    Use indent of 3 for TS/JSON, fix lint issues

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,20 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-const queryClient = new QueryClient()
-const rootElem = document.getElementById('root');
+const queryClient = new QueryClient();
+const rootElem = document.getElementById("root");
 
 if (rootElem === null) {
-    throw new Error("No root element found")
+   throw new Error("No root element found");
 }
 
 createRoot(rootElem).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </StrictMode>,
-)
+   <StrictMode>
+      <QueryClientProvider client={queryClient}>
+         <App />
+      </QueryClientProvider>
+   </StrictMode>,
+);
