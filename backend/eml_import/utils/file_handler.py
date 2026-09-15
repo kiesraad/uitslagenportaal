0188bfad72f5ddab0b5b52e37e40b4d27fb6dc81
Corniël Joosse <corniel@pixento.nl>
2026-09-15 16:15:39 +0200

    First steps for adding a CSV importer

--- backend/eml_import/utils/file_handler.py
+++ backend/eml_import/utils/file_handler.py
@@ -18,6 +18,7 @@ from eml_import.utils.eml_110_importer import EML110aImporter
 from eml_import.utils.eml_230_importer import EML230bImporter
 from eml_import.utils.eml_510_importer import EML510bImporter, EML510cImporter, EML510dImporter
 from eml_import.utils.eml_base_importer import EMLBaseImporter
+from eml_import.utils.named_bytes_io import NamedBytesIO
 from mainsite.utils.eml_type import EmlType
 
 
@@ -36,13 +37,16 @@ def build_parser() -> XmlParser:
 
 
 class BaseFileHandler(ABC):
-    _DOCUMENT_TYPES: dict[str, tuple[type[Emlstructure], type[EMLBaseImporter]]] = {
+    _DOCUMENT_TYPES: dict[str, tuple[type[Emlstructure | None], type[EMLBaseImporter]]] = {
         EmlType.EML_110a: (Eml110a, EML110aImporter),  # Verkiezingsdefinitie
         EmlType.EML_230b: (Eml230, EML230bImporter),  # Kandidatenlijst
         EmlType.EML_510b: (Eml510, EML510bImporter),  # Telling
         EmlType.EML_510c: (Eml510, EML510cImporter),  # Telling HSB
         EmlType.EML_510d: (Eml510, EML510dImporter),  # Totaaltelling
+        # ElectionDocument.FileType.CSV_GSB: (None, CSV510bImporter),
     }
+
+    _VALID_EXTENSIONS = [".xml", ".csv"]
 
     def __init__(self):
         self.logger = logging.getLogger(f"{self.__class__.__name__}")
@@ -66,13 +70,17 @@ class BaseFileHandler(ABC):
         xml_file_path.seek(0)
         return cls._root_element_id(xml_file_path)
 
-    def _classify_files[T = Path | BytesIO](self, input_files: list[T]) -> dict[str, list[T]]:
-        xml_files: dict[str, list[T]] = {key: [] for key in self._DOCUMENT_TYPES}
-        for xml_file_path in input_files:
-            document_id = self._document_id(xml_file_path)
-            if document_id and document_id in xml_files:
-                xml_files[document_id].append(xml_file_path)
-        return xml_files
+    def _classify_files[T = Path | NamedBytesIO](self, input_files: list[T]) -> dict[str, list[T]]:
+        files: dict[str, list[T]] = {key: [] for key in self._DOCUMENT_TYPES}
+        for file_path in input_files:
+            match file_path.suffix:
+                case ".xml":
+                    document_id = self._document_id(file_path)
+                    if document_id and document_id in files:
+                        files[document_id].append(file_path)
+                case ".csv":
+                    ...
+        return files
 
     @abstractmethod
     def run(self) -> tuple[int, bool]:
