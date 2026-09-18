a469e462e2a138a187be3cf5fc16e1a6ed42cf9a
Corniël Joosse <corniel@pixento.nl>
2026-09-18 15:46:54 +0200

    Improve responsiveness HomePage and ReportIssuePage, add href support to Button

--- frontend/src/components/HomePage/HeroGrid.tsx
+++ frontend/src/components/HomePage/HeroGrid.tsx
@@ -2,9 +2,13 @@ import KiesraadGridHexagon from "@/elements/KiesraadGridHexagon.tsx";
 
 export function HeroGrid() {
    return (
-      <div className={"home-page-hero-right"}>
-         <div className={"home-page-hero-img-container"}>
-            <img src="/images/homepage_img.webp" alt="Home hero" className={"home-page-hero-img"} />
+      <div className="-order-1 flex flex-col justify-center lg:order-0">
+         <div className="flex justify-center pb-10 lg:pb-0 xl:pr-20">
+            <img
+               src="/images/homepage_img.webp"
+               alt="Home hero"
+               className="relative z-10 aspect-video max-h-80 w-full object-cover lg:aspect-11/12 lg:max-h-140 lg:max-w-2xl"
+            />
          </div>
          <div className="hero-grid-container text-blue-500">
             <div className={"hero-grid-left"}>
