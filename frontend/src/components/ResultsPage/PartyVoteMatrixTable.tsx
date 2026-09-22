2f6131f4c3d0362dce867753dbbe0749137081be
Corniël Joosse <corniel@pixento.nl>
2026-09-22 11:25:46 +0200

    Make PartyVoteMatrixTable candidate not sticky on mobile

--- frontend/src/components/ResultsPage/PartyVoteMatrixTable.tsx
+++ frontend/src/components/ResultsPage/PartyVoteMatrixTable.tsx
@@ -22,15 +22,15 @@ export default function PartyVoteMatrixTable({ matrix }: Props) {
       <div className="mt-4 max-w-full overflow-x-auto">
          <table className="border-separate border-spacing-0 whitespace-nowrap [&_td]:p-4 [&_th]:p-4">
             <thead>
-               <tr className="font-bold bg-white text-left align-middle">
-                  <th className="border-r border-b border-gray-200 sticky left-0 bg-inherit z-20">
+               <tr className="bg-white text-left align-middle font-bold">
+                  <th className="z-20 border-gray-200 border-r border-b bg-inherit sm:sticky sm:left-0">
                      <Trans>Kandidaat</Trans>
                   </th>
-                  <th className="min-w-24 border-b border-gray-200 bg-inherit">
+                  <th className="min-w-24 border-gray-200 border-b bg-inherit">
                      <Trans>Totaal</Trans>
                   </th>
                   {matrix.columns.map((column) => (
-                     <th key={column.slug} className="min-w-24 border-b border-gray-200 bg-inherit">
+                     <th key={column.slug} className="min-w-24 border-gray-200 border-b bg-inherit">
                         {column.region_name}
                      </th>
                   ))}
@@ -39,8 +39,8 @@ export default function PartyVoteMatrixTable({ matrix }: Props) {
             <tbody>
                {matrix.rows.map(({ candidate, total, votes }) => (
                   <tr key={candidate.position} className="bg-white odd:bg-blue-50">
-                     <td className="border-r border-gray-200 bg-inherit sticky left-0">
-                        <div className="flex items-center gap-3.5">
+                     <td className="border-gray-200 border-r bg-inherit sm:sticky sm:left-0">
+                        <div className="flex items-center sm:gap-3.5">
                            <span className="min-w-6 text-gray-500">{candidate.position}</span>
                            <span>{formatCandidateName(candidate)}</span>
                         </div>
@@ -53,19 +53,19 @@ export default function PartyVoteMatrixTable({ matrix }: Props) {
                      ))}
                   </tr>
                ))}
-               <tr className="border-t border-gray-200 bg-white">
-                  <td className="border-r border-t border-gray-200 bg-inherit sticky left-0">
+               <tr className="border-gray-200 border-t bg-white">
+                  <td className="border-gray-200 border-t border-r bg-inherit sm:sticky sm:left-0">
                      <div className="flex items-center gap-3.5">
                         <span className="font-bold">
                            <Trans>Totaal</Trans>
                         </span>
                      </div>
                   </td>
-                  <td className="min-w-20 bold font-number border-t border-gray-200">
+                  <td className="bold min-w-20 border-gray-200 border-t font-number">
                      {formatVotes(matrix.totals.total)}
                   </td>
                   {matrix.columns.map((column) => (
-                     <td key={column.slug} className="min-w-16 font-number border-t border-gray-200">
+                     <td key={column.slug} className="min-w-16 border-gray-200 border-t font-number">
                         {formatVotes(matrix.totals.votes[column.slug])}
                      </td>
                   ))}
